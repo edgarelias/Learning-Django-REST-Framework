192b794ba30059f74ac9e5fba4c9fac4d179d820
eeliasZabu <eelias@zabu.io>
2020-08-31 18:23:31 -0500

    serialization

---
+++ snippets/tests.py
@@ -0,0 +1,4 @@
+from django.test import TestCase
+
+# Create your tests here.
+# http://127.0.0.1:8000/snippets/1
--- tutorial/settings.py
+++ tutorial/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # own
+    'snippets.apps.SnippetsConfig',
 
     # third party
     'rest_framework',
