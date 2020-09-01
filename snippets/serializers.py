d4dfbb6d004b17f3e4e337c44aed41a1db468a80
eeliasZabu <eelias@zabu.io>
2020-09-01 12:48:52 -0500

    associate snippets with users

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,40 +1,19 @@
 from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+from django.contrib.auth.models import User
+from rest_framework import permissions
 
-# We need provide a way of serializing and deserializing the 
-# snippet instances into representations such as json. 
-
-# class SnippetSerializer(serializers.Serializer):
-#     id = serializers.IntegerField(read_only=True)
-#     title = serializers.CharField(required=False, allow_blank=True, max_length=100)
-#     code = serializers.CharField(style={'base_template': 'textarea.html'})
-#     linenos = serializers.BooleanField(required=False)
-#     language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
-#     style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
-
-#     def create(self, validated_data):
-#         """
-#         Create and return a new `Snippet` instance, given the validated data.
-#         """
-#         return Snippet.objects.create(**validated_data)
-
-#     def update(self, instance, validated_data):
-#         """
-#         Update and return an existing `Snippet` instance, given the validated data.
-#         """
-#         instance.title = validated_data.get('title', instance.title)
-#         instance.code = validated_data.get('code', instance.code)
-#         instance.linenos = validated_data.get('linenos', instance.linenos)
-#         instance.language = validated_data.get('language', instance.language)
-#         instance.style = validated_data.get('style', instance.style)
-#         instance.save()
-#         return instance
-
-# It's important to remember that ModelSerializer classes don't do anything particularly magical, 
-# they are simply a shortcut for creating serializer classes:
-# - An automatically determined set of fields.
-# - Simple default implementations for the create() and update() methods.
 class SnippetSerializer(serializers.ModelSerializer):
     class Meta:
         model = Snippet
+        owner = serializers.ReadOnlyField(source='owner.username')
+        permission_classes = [permissions.IsAuthenticatedOrReadOnly]
         fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
+
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+
+    class Meta:
+        model = User
+        permission_classes = [permissions.IsAuthenticatedOrReadOnly] # which will ensure that authenticated requests get read-write access, and unauthenticated requests get read-only access.
+        fields = ['id', 'username', 'snippets']
